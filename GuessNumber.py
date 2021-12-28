b023db77c9691b29f50e8e6da037de0dc698295d
Chia-Hao Chang <cchang82@usc.edu>
2021-12-27 21:54:20 -0800

    4th upload

--- GuessNumber.py
+++ GuessNumber.py
@@ -12,5 +12,24 @@ def guessNumber(upperLimit):
 			print("Guess a lower number.")
 	print(f"Congratulations! You have guessed the number {theNumber} correctly.")
 
+#computer guess
+def computerGuess(upperLimit):
+	lowerBound = 1
+	upperBound = upperLimit
+	result = ""
+	while result != "C":
+		guess = random.randint(lowerBound, upperBound)
+		result = input(f"Is {guess} too high (H), too low (L), or correct (C)?")
+		if result == "H":
+			upperBound = guess - 1
+		elif result == "L":
+			lowerBound = guess + 1
+	print(f"Computer has guessed your number, {guess}, correctly.")
+
 if __name__ == "__main__":
-	guessNumber(30)
+	playWithComputer = input(f"Would you like to play with computer? (Y/N): ")
+	upperLimit = int(input(f"Please type in an integer number to decide your upper limit: "))
+	if playWithComputer == "Y":
+		computerGuess(upperLimit)
+	else:
+		guessNumber(upperLimit)
