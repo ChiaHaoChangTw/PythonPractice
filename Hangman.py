c57a85ebb6db38d6ee4d041ffe59f7cfd41ac6db
Chia-Hao Chang <cchang82@usc.edu>
2022-01-03 18:33:29 -0800

    add hangman visual

--- Hangman.py
+++ Hangman.py
@@ -1,6 +1,7 @@
 import random
 from WordsForHangman import words
 import string
+from HangmanVisual import livesVisualDict
 
 def printWords(words):
 	print(words)
@@ -17,6 +18,16 @@ def playHangman():
 	allChars = set(string.ascii_lowercase)
 	guessedChars = set()
 	lives = 7
+	while len(wordChars) > 0 and lives > 0:
+		print("You have ", lives, " lives left, and you have used these characters: ", " ".join(guessedChars))
+		result = []
+		for wordChar in word:
+			if wordChar in gussedChars:
+				result.add(wordChar)
+			else:
+				result.add("_")
+		print(livesVisualDict[lives])
+		print("Current result: ", " ".join(result))
 
 if __name__ == "__main__":
 	playHangman()
