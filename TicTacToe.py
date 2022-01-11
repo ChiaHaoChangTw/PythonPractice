b519fd25c142572baf222e30cea72a86b334b24e
Chia-Hao Chang <cchang82@usc.edu>
2022-01-10 23:33:06 -0800

    tictactoe 3rd upload

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,3 +1,6 @@
+import math
+from TicTacToePlayer import HumanPlayer, ComputerPlayer
+
 class TicTacToe():
 	def __init__(self):
 		self.board = self.makeBoard()
@@ -17,5 +20,44 @@ class TicTacToe():
 		for rowIndex in boardIndex:
 			print("|", " | ".join(rowIndex), "|")
 
-	def emptyIndex():
+	def makeMove(self, index, player):
+		if self.board[index] == " ":
+			self.board[index] = player
+			if self.isWin(index, player):
+				self.winner = player
+			return True
+		return False
+
+	def isWin(self, index, player):
+		rowIdx = math.floor(index / 3)
+		currRow = self.board[rowIdx * 3 : (rowIdx + 1) * 3]
+		if all([i == player for i in currRow]):
+			return True
+		colIdx = index % 3
+		currCol = [self.board[colIdx + i * 3] for i in range(3)]
+		if all([i == player for i in currCol]):
+                        return True
+		if index % 2 == 0:
+			diagonal1 = [self.board[i] for i in [0, 4, 8]]
+			if all([i == player for i in diagonal1]):
+                        	return True
+			diagonal2 = [self.board[i] for i in [2, 4, 6]]
+			if all([i == player for i in diagonal2]):
+                        	return True
+		return False
+
+	def emptyIndex(self):
 		return [i for i, index in enumerate(self.board) if index == " "]
+	
+	def hasEmptyIndex(self):
+		return " " in self.board
+
+	def numEmptyIndex(self):
+		return self.board.count(" ")
+
+def play(game, xPlayer, oPlayer, printGame = True):
+	if printGame:
+		game.printBoardIndex()
+	player = "x"
+	while game.hasEmptyIndex():
+		pass
--- TicTacToePlayer.py
+++ TicTacToePlayer.py
@@ -11,7 +11,18 @@ class HumanPlayer(Player):
 	def __init__(self, symbol):
 		super().__init__(symbol)
 	def getMove(self, game):
-		pass
+		validIdx = False
+		idx = None
+		while not validIdx:
+			in = input(self.symbol + "\'s turn. Please enter move (0 ~ 9): ")
+			try: 
+				idx = int(in)
+				if idx not in game.emptyIndex():
+					raise ValueError
+				validIdx = true
+			except ValueError:
+				print("Please enter valid move. Try again.")
+		return idx 
 
 class ComputerPlayer(Player):
 	def __init__(self, symbol):
