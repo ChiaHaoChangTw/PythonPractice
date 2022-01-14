058906514f00bc8f11de4e7ac782e51d702a9384
Chia-Hao Chang <cchang82@usc.edu>
2022-01-14 02:35:56 -0800

    Minesweeper 1st commit

---
+++ MineSweeper.py
@@ -0,0 +1,35 @@
+import random
+import re
+
+class Board:
+	def init(self, boardSize, numMine):
+		pass
+	
+	def makeBoard(self):
+		pass
+	
+	def initBoardValue(self):
+		pass
+	
+	def numNeighborMine(self):
+		pass
+	
+	def discover(self, row, col):
+		pass
+	
+	def toString(self):
+		pass
+
+def playGame():
+	pass
+
+if __name__ == "__main__":
+	playGame()
+	
+
+
+
+
+
+
+
