db2ed8c34ad44033751959ae719e23cc03968855
Chia-Hao Chang <cchang82@usc.edu>
2022-01-16 19:19:47 -0800

    Minesweeper 3rd commit

--- MineSweeper.py
+++ MineSweeper.py
@@ -23,10 +23,21 @@ class Board:
 		return board
 	
 	def initBoardValue(self):
-		pass
+		for i in range(self.boardSize)
+			for j in range(self.boardSize)
+				if self.board[i][j] == "*":
+					continue
+				self.board[i][j] = self.numNeighborMine(i, j)
 	
-	def numNeighborMine(self):
-		pass
+	def numNeighborMine(self, row, col):
+		num = 0
+		for i in range(max(0, row - 1), min(self.boardSize - 1, row + 1) + 1):
+			for j in range(max(0, col - 1), min(self.boardSize - 1, col + 1) + 1):
+				if i == row and j = col:
+					continue
+				if self.board[i][j] == "*":
+					num += 1
+		return num
 	
 	def discover(self, row, col):
 		pass
